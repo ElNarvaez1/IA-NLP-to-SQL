eeb7ad91a3e5713753df8d1fceb4b6947fe3c247
Alexis Narvaez Ruiz <zaizealexis@gmail.com>
2022-06-10 23:41:05 -0500

    Cambios de veras que si

---
+++ Main.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+//const data  = require('./data/clearDataJSON.json');
+const KEY_SECRET = "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";
+const { Configuration, OpenAIApi } = require("openai");
+
+// express
+app.use(express.static("public"));
+app.use(express.json());
+
+// OpenIA
+const configuration = new Configuration({
+  apiKey: KEY_SECRET,
+});
+
+const getResponse = async (nlp) => {
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: "# En PostgreSQL, " + nlp+':\n',
+    temperature: 0.3,
+    max_tokens: 80,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  return response.data;
+};
+
+// Enrutador alv
+app.get("/", (req, res) => {
+  res.send("index.html");
+});
+
+app.get("/data", (req, res) => {
+  res.send("Método GET :)");
+});
+
+app.post("/data", (req, res) => {
+  //console.log(req.body);
+  getResponse(req.body.nlp)
+    .then((e) => e)
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
+document.addEventListener("DOMContentLoaded", function () {
+  let containerResult = document.getElementById("information-generate");
+  let buttonGenerate = document.getElementById("generate");
+  buttonGenerate.addEventListener("click", (e) => {
+    e.preventDefault();
+    let nlp = document.getElementById("nlp").value;
+    let url = "http://localhost:5000/data";
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ nlp: nlp }),
+    })
+      .then((e) => e.json())
+      .then((data) => {
+        containerResult.innerHTML = data.choices[0].text
+      });
+  });
 });
