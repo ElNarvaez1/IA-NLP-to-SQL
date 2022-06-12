d1c4fe8ff3636f525f5d9ed98661c477f7e66146
Alexis Narvaez Ruiz <zaizealexis@gmail.com>
2022-06-11 19:21:17 -0500

    Creo que si esta chido

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //Elmentos del HTML
   let containerResult = document.getElementById("information-generate");
   let buttonGenerate = document.getElementById("generate");
+  //Evento del boton
   buttonGenerate.addEventListener("click", (e) => {
     e.preventDefault();
+    //Obtener el valor del input
     let nlp = document.getElementById("nlp").value;
+    //url de la api
     let url = "http://localhost:5000/data";
+    //Crear el objeto para renderizar markdown
+    var md = window.markdownit({
+      highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(str, { language: lang }).value;
+          } catch (__) {}
+        }
+
+        return ""; // use external default escaping
+      },
+    });
 
     fetch(url, {
       method: "POST",
@@ -15,7 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
     })
       .then((e) => e.json())
       .then((data) => {
-        containerResult.innerHTML = data.choices[0].text
+        var result = md.render("```sql\n" + data.choices[0].text + "\n```");
+        containerResult.innerHTML = result;
+        //containerResult.innerHTML += result
       });
   });
 });
